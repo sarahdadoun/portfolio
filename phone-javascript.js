59e519b8148d537956f51a9ddd3eab7fc9d8f92b
sarahdadoun <85398444+sarahdadoun@users.noreply.github.com>
2021-07-18 15:31:23 -0400

    removed the js file and just added <script> because it wasnt working

--- phone-javascript.js
+++
@@ -1,5 +0,0 @@
-let p = document.getElementById("welcome-message").target;
-
-if($(window).width() < 500){
-    p = "You've landed on Sarah Dadoun's online portfolio, which is still being created as we speak! &#128522 Come back in a month and check it out!";
-}
